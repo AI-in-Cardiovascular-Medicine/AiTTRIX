72903ffa0748ae80b2e67759f9badebe3f9f0d29
giovabaj <giovanni.baj2@gmail.com>
2026-03-06 10:20:43 +0100

    Add make_plots_tables to the overall pipeline

--- run_all.py
+++ run_all.py
@@ -1,6 +1,9 @@
-import os
+import subprocess
+import sys
 
 for config_name in ["rsf", "nac"]:
     print("\nRunning config:", config_name)
-    cmd = f'python train_config.py --config-name {config_name}'
-    os.system(cmd)
+    subprocess.run([sys.executable, "train_config.py", f"--config-name={config_name}"], check=True)
+
+print("\nMake plots and tables from results")
+subprocess.run([sys.executable, "make_plots_tables.py"], check=True)
